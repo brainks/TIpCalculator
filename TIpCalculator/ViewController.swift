8825bdcd2ec58f4aaa6ebb481ba0d2ba5a9a53d1
Philomene Jean Lucien <philo@philos-macbook-air.local>
2018-08-24 12:31:22 -0400

    Initial Commit

---
+++ TIpCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIpCalculator
+//
+//  Created by Philo on 24/08/2018.
+//  Copyright © 2018 BrainkSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
